f4a359d4eb9af2e14052593efee62817a00d30e4
Adrian-Tudor Panescu <adrian@panescu.com>
2014-03-08 19:33:27 +0100

    source code appendix content
    Signed-off-by: Adrian-Tudor Panescu <adrian@panescu.com>

--- git_summary.sh
+++ git_summary.sh
@@ -3,5 +3,5 @@
 cd ~/src/invenio/ &&
 git fetch invenio-jiri &&
 rm ~/src/thesis/static/lst/pu_commits.txt &&
-git log --author="Panescu" invenio-jiri/pu > ~/src/thesis/static/lst/pu_commits.txt &&
+git log --author="Panescu" --pretty="format: SHA    %h%nAuthor %an <%ae>%nDate   %ai%n%n%B" --stat invenio-jiri/pu > ~/src/thesis/static/lst/pu_commits.txt &&
 cd -
