1006a6d3d44b1ea462a1aa6625ee0eb04c49ab91
Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>
2014-03-13 15:59:25 +0100

    shorter git summary
    Reported-by: Tibor Simko <tibor.simko@cern.ch>
    Signed-off-by: Adrian-Tudor Panescu <adrian.tudor.panescu@cern.ch>

--- git_summary.sh
+++ git_summary.sh
@@ -3,5 +3,9 @@
 cd ~/src/invenio/ &&
 git fetch invenio-jiri &&
 rm ~/src/thesis/static/lst/pu_commits.txt &&
-git log --author="Panescu" --pretty="format: SHA    %h%nAuthor %an <%ae>%nDate   %ai%n%n%B" --stat invenio-jiri/pu > ~/src/thesis/static/lst/pu_commits.txt &&
+echo "X commits with X additions and X deletions:" > ~/src/thesis/static/lst/pu_commits.txt &&
+git log --author="Panescu" --oneline invenio-jiri/pu | wc -l >>  ~/src/thesis/static/lst/pu_commits.txt &&
+git log --author="Panescu" --pretty=tformat: --numstat invenio-jiri/pu | gawk '{ add += $1 ; subs += $2 ; } END { printf "%s\n%s\n",add,subs }' - >>  ~/src/thesis/static/lst/pu_commits.txt &&
+git log --author="Panescu" --format="%h %s (%ci)" invenio-jiri/pu >> ~/src/thesis/static/lst/pu_commits.txt &&
+vim ~/src/thesis/static/lst/pu_commits.txt &&
 cd -
